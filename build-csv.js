0596cb9a12b493e4825ccde17b4ce041beeb570f
Madison Hanberry <madison.hanberry@verygoodsecurity.com>
2022-04-06 20:56:19 -0700

    add user feedback + complete TODOs + remove duplicates

--- build-csv.js
+++ build-csv.js
@@ -4,20 +4,23 @@ const securityGroups = JSON.parse(fs.readFileSync('security-groups.json', 'ascii
 
 const networkInterfaces = JSON.parse(fs.readFileSync('network-interfaces.json', 'ascii')).NetworkInterfaces
 
-let pciTable = [[
-	'Firewall', 'Source', 'Destination', 'Ports',
-	'Protocols', 'Business Justification', 'Risks', 'Mitigation Controls'
-]]
+let pciTableSet = new Set([
+	'"Firewall","Source","Destination","Ports",' +
+	'"Protocols","Business Justification","Risks","Mitigation Controls"'
+])
 
 // Aggregate PCI information
 securityGroups.forEach(securityGroup => {
 	const matchingInterfaces = networkInterfaces.filter(networkInterface =>
 		networkInterface.Groups.some(group =>
-			group.GroupId == securityGroup.GroupId
+			group.GroupId === securityGroup.GroupId
 		)
 	)
+
+	// Check if any interfaces belong to the security group
+	if (matchingInterfaces.length === 0) return
 	
-	const PrivateIpAddresses = matchingInterfaces.map(networkInterface =>
+	const privateIpAddresses = matchingInterfaces.map(networkInterface =>
 		networkInterface.PrivateIpAddresses.map(ip =>
 			ip.PrivateIpAddress
 		)
@@ -26,51 +29,103 @@ securityGroups.forEach(securityGroup => {
 	.sort()
 	.join('\n')
 
+	const publicIpAddresses = matchingInterfaces.filter(networkInterface =>
+		networkInterface.Association?.PublicIp != undefined
+	)
+	.map(networkInterface =>
+		networkInterface.Association?.PublicIp
+	)
+	.sort()
+	.join('\n')
+
 	// Gather ingress information
 	securityGroup.IpPermissions.forEach(ipPermission => {
-		const firewall = 'TODO'
-		const source = 'TODO'
-		const destination = PrivateIpAddresses
-		const ports = ipPermission.FromPort
+		const privateFirewall = 'Internal'
+		const source = ipPermission
+			.IpRanges.map(ipRange => ipRange.CidrIp)
+			.concat(ipPermission.UserIdGroupPairs
+				.map(pair => `UserIdGroupPair ${pair.GroupId}/${pair.UserId}`)
+			)
+			.join('\n')
+		const destination = privateIpAddresses
+		const ports = ipPermission.toPort === undefined ?
+			'all' : ipPermission.toPort
 		const protocols = ipPermission.IpProtocol
 		const businessJustification = securityGroup.Description
 		const risks = ''
 		const mitigationControls = ''
-
 		
-		pciTable.push([
-			firewall, source, destination, ports,
-			protocols, businessJustification, risks, mitigationControls
-		])
+		pciTableSet.add(
+			[
+				privateFirewall, source, destination, ports,
+				protocols, businessJustification, risks, mitigationControls
+			]
+			.map(column => `"${column}"`)
+			.join(',')
+		)
+
+		// Check if there are public ip addresses
+		if(publicIpAddresses === '') return
+		
+		const publicFirewall = 'Internet'
+		const publicDestination = publicIpAddresses
+
+		pciTableSet.add(
+			[
+				publicFirewall, source, publicDestination, ports,
+				protocols, businessJustification, risks, mitigationControls
+			]
+			.map(column => `"${column}"`)
+			.join(',')
+		)
 	})
 
 	// Gather egress information
 	securityGroup.IpPermissionsEgress.forEach(ipPermission => {
-		const firewall = 'TODO'
-		const source = PrivateIpAddresses
-		const destination = 'TODO'
-		const ports = ipPermission.FromPort
-		const protocols = ipPermission.IpProtocol == -1 ?
+		const privateFirewall = 'Internal'
+		const source = privateIpAddresses
+		const destination = ipPermission
+			.IpRanges.map(ipRange => ipRange.CidrIp)
+			.concat(ipPermission.UserIdGroupPairs
+				.map(pair => `UserIdGroupPair ${pair.GroupId}/${pair.UserId}`)
+			)
+			.join('\n')
+		const ports = ipPermission.FromPort === undefined ?
+			'all' : ipPermission.FromPort
+		const protocols = ipPermission.IpProtocol === -1 ?
 			'all' : ipPermission.IpProtocol
 		const businessJustification = securityGroup.Description
 		const risks = ''
 		const mitigationControls = ''
 
+		pciTableSet.add(
+			[
+				privateFirewall, source, destination, ports,
+				protocols, businessJustification, risks, mitigationControls
+			]
+			.map(column => `"${column}"`)
+			.join(',')
+		)
+
+		// Check if there are public ip addresses
+		if(publicIpAddresses === '') return
 		
-		pciTable.push([
-			firewall, source, destination, ports,
-			protocols, businessJustification, risks, mitigationControls
-		])
+		const publicFirewall = 'Internet'
+		const publicSource = publicIpAddresses
+
+		pciTableSet.add(
+			[
+				publicFirewall, publicSource, destination, ports,
+				protocols, businessJustification, risks, mitigationControls
+			]
+			.map(column => `"${column}"`)
+			.join(',')
+		)
 	})
 })
 
 // Create CSV
-const pciTableCsv = pciTable.map(row =>
-	row.map(column =>
-		`"${column}"`
-	)
-	.join(',')
-)
-.join('\n')
+const pciTableCsv = Array.from(pciTableSet)
+	.join('\n')
 
 fs.writeFileSync('pci-table.csv', pciTableCsv)
--- main.sh
+++ main.sh
@@ -14,10 +14,12 @@ login(){
 }
 
 get_security_groups(){
+	echo 'retrieving security groups...'
 	aws --profile $PROFILE ec2 describe-security-groups --output json > security-groups.json
 }
 
 get_network_interfaces(){
+	echo 'retrieving network interfaces...'
 	aws --profile $PROFILE ec2 describe-network-interfaces --output json > network-interfaces.json
 }
 
@@ -26,4 +28,5 @@ get_network_interfaces(){
 login
 get_security_groups
 get_network_interfaces
+echo 'building pci-table.csv...'
 node build-csv.js
